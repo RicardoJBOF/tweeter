3eaa05d6e0c69dc2243c60f2987da57b0ba18782
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-09-10 22:27:54 +0000

    Submit Project

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Tweeter is a simple, single-page Twitter clone.
 
 This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express and MongoDB back-end skills.
 
+## Final Product
+
+screenshot 1 - /urls image
+screenshot 2 - /urls/new
+screenshot 3 - /login image
+
 ## Getting Started
 
 1. Fork this repository, then clone your fork of this repository.
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,35 +9,7 @@ $(document).ready( function() {
 
 
 
-//TESTING PART 1
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-
 const renderTweets = function(tweets) {
-  //$('.container-body').empty(); //AFTER SEND A TWEET, THE SENDING BOX WILL DESAPPEAR
 
   //LOOP MESSAGES
   //REVER IS A PROPERTY TO LOOP MESSAGE FROM NEW TO OLD ONES 
